2e47c46358fada8a4340acf2a8bba66d23f1e6a2
dangquangdon <dangquangdon@gmail.com>
2019-02-10 17:27:41 +0200

    add Part 2

--- FlaskWeb/flaskApp/run.py
+++ FlaskWeb/flaskApp/run.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
+posts = [
+  {
+    'author':'Don',
+    'title': "Test post 1",
+    'content': "Test content 1"
+  },
+  {
+    'author':'Don',
+    'title': "Test post 2",
+    'content': "Test content 2"
+  },
+  {
+    'author':'Don',
+    'title': "Test post 3",
+    'content': "Test content 3"
+  },
+]
+
+
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template("home.html", posts = posts)
 
 @app.route("/about")
 def about():
-    return "About"
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
