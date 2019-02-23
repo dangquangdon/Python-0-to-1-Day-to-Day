dbd492b64e7f70d524e5cbc56448967c27d96a5b
dangquangdon <dangquangdon@gmail.com>
2019-02-23 15:34:43 +0200

    change debug to false

--- FlaskWeb/flaskApp/config/prod.py
+++ FlaskWeb/flaskApp/config/prod.py
@@ -5,7 +5,7 @@ with open('/etc/config.json') as config_file:
     config = json.load(config_file)
 
 
-DEBUG = True
+DEBUG = False
 SECRET_KEY = config.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
